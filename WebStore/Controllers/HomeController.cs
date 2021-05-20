452b69b5d2ff4491ce9af704be86f3bc1db96872
Infarh <shmachilin@yandex.ru>
2021-05-20 21:00:29 +0300

    Сотрудники магазина

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     //[Controller]
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration) { _Configuration = Configuration; }
@@ -22,6 +31,12 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content(_Configuration["Greetings"]);
+            //return View("Index");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }

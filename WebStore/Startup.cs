75961ca379207b0f0d9ad3466ab74f63ecfa6a0c
Infarh <shmachilin@yandex.ru>
2021-05-20 20:29:26 +0300

    Добавлена конфигурация в класс Startup

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,13 @@ namespace WebStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -21,11 +29,14 @@ namespace WebStore
 
             app.UseRouting();
 
+
+            //var greetings = Configuration["Greetings"];
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    //await context.Response.WriteAsync(greetings);
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
             });
         }

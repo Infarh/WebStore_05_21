2806ae4f62cd5d94c1a01c3bf4dbe628b0986ae2
Infarh <shmachilin@yandex.ru>
2021-05-28 20:27:51 +0300

    Частные случаи маршрутов

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    //[Route("Staff")]
+    //[Route("Employees")]
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+        //[Route("all")]
+        public IActionResult Index() => View(__Employees);
+
+        //[Route("info/{id}")]
+        //[Route("info-id-{id}")]
+        public IActionResult Details(int id)
+        {
+            var employee = __Employees.FirstOrDefault(employe => employe.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,14 +9,6 @@ namespace WebStore.Controllers
     //[Controller]
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-        };
-
-
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
@@ -30,21 +22,6 @@ namespace WebStore.Controllers
         {
             return Content(_Configuration["Greetings"]);
             //return View("Index");
-        }
-
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }
-
-        public IActionResult EmployeDetails(int id)
-        {
-            var employee = __Employees.FirstOrDefault(employe => employe.Id == id);
-
-            if (employee == null)
-                return NotFound();
-
-            return View(employee);
         }
 
         public IActionResult Blog() => View();

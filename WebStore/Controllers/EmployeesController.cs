9a71743ea7b632a99bce8e56c859ea0409c60752
Infarh <shmachilin@yandex.ru>
2021-05-28 21:08:47 +0300

    Логика редактирования сотрудников в контроллере

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -30,11 +30,14 @@ namespace WebStore.Controllers
             return View(employee);
         }
 
-        public IActionResult Create() => View();
+        public IActionResult Create() => View("Edit", new EmployeeViewModel());
 
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int? id)
         {
-            var employee = _EmployeesData.Get(id);
+            if (id is null)
+                return View(new EmployeeViewModel());
+
+            var employee = _EmployeesData.Get((int)id);
             if (employee is null) 
                 return NotFound();
 
@@ -61,7 +64,10 @@ namespace WebStore.Controllers
                 Age = Model.Age,
             };
 
-            _EmployeesData.Update(employee);
+            if (employee.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Update(employee);
 
             return RedirectToAction("Index");
         }

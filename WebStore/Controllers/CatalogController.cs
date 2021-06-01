68fdfa703780ddc7cf706d4082e287bbe24bce62
Infarh <shmachilin@yandex.ru>
2021-06-01 21:46:45 +0300

    Контроллер каталога товаров

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -16,7 +18,28 @@ namespace WebStore.Controllers
 
         public IActionResult Index(int? BrandId, int? SectionId)
         {
-            return View();
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products
+                   .OrderBy(p => p.Order)
+                   .Select(p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl,
+                    })
+            });
         }
     }
 }

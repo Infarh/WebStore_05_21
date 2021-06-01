e15ab8c08eff2cd31d1699718aef00b3e0434684
Infarh <shmachilin@yandex.ru>
2021-06-01 21:31:21 +0300

    Подготовка представления каталога товаров

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index(int? BrandId, int? SectionId)
+        {
+            return View();
+        }
+    }
+}

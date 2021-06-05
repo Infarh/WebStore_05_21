9929010e2078c692930368610c68f484e788c6d0
Infarh <shmachilin@yandex.ru>
2021-06-05 20:27:08 +0300

    Подключение контекста БД в основном приложении в качестве сервиса

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -2,10 +2,11 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using WebStore.DAL.Context;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.MiddleWare;
 using WebStore.Services;
@@ -21,6 +22,9 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt => 
+                opt.UseSqlServer(Configuration.GetConnectionString("MSSQL")));
+
             services.AddSingleton<IEmployeesData, InMemoryEmployesData>();  // Объект InMemoryEmployesData создаётся один раз на всё время работы приложения
             services.AddSingleton<IProductData, InMemoryProductData>();
 

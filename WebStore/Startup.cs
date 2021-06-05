2827fe0bd7c5f91e52bd53c30e893b8719e4a044
Infarh <shmachilin@yandex.ru>
2021-06-05 20:22:35 +0300

    Удаление хлама

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,18 +21,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<ITestService, TestService>();
-            //services.AddScoped<IPrinter, DebugPrinter>();
-
             services.AddSingleton<IEmployeesData, InMemoryEmployesData>();  // Объект InMemoryEmployesData создаётся один раз на всё время работы приложения
-            // Нужен если сервис должен хранить состояние на время работы приложения
-
-            //services.AddScoped<IEmployeesData, InMemoryEmployesData>();     // Объект создаётся единожды для области
-            // Если нужен сервис, который обладает памятью только в пределах обработки одного запроса
-
-            //services.AddTransient<IEmployeesData, InMemoryEmployesData>();  // Объект InMemoryEmployesData создаётся каждый раз заново
-            // Когда сервис не подразумевает наличие внутренней памяти
-
             services.AddSingleton<IProductData, InMemoryProductData>();
 
             services.AddControllersWithViews(opt => opt.Conventions.Add(new TestControllersConvention()))
@@ -42,20 +30,6 @@ namespace WebStore
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
-            //var test_service = services.GetRequiredService<ITestService>();
-            //test_service.Test();
-
-
-            //var employees1 = services.GetService<IEmployeesData>();
-            //var employees2 = services.GetService<IEmployeesData>();
-
-            //IEmployeesData employyes3;
-
-            //using (var scope = services.CreateScope())
-            //    employyes3 = scope.ServiceProvider.GetService<IEmployeesData>();
-
-            //var is_equals = ReferenceEquals(employees1, employyes3);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -67,74 +41,19 @@ namespace WebStore
 
             app.UseMiddleware<TestMiddleWare>();
 
-            //app.Use(async (context, next) =>
-            //{
-            //    await next();
-            //});
-
-            //app.Map("/TestMapRequest", opt => opt.Run(async context =>
-            //{
-            //    await Task.Delay(100);
-            //    var stream_writer = new StreamWriter(context.Response.Body);
-            //    await stream_writer.WriteAsync("Hello from TestMapRequest");
-            //    await context.Response.CompleteAsync();
-            //}));
-
             app.UseWelcomePage("/WelcomePage");
 
-            //var greetings = Configuration["Greetings"];
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/greetings", async context =>
                 {
-                    //await context.Response.WriteAsync(greetings);
                     await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
 
-                //endpoints.MapDefaultControllerRoute();
                 endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-    }
-
-    interface ITestService
-    {
-        void Test();
-    }
-
-
-    class TestService : ITestService
-    {
-        private IPrinter _Printer;
-
-        public TestService(IPrinter Printer)
-        {
-            _Printer = Printer;
-        }
-
-        public void Test()
-        {
-            _Printer.Print("Запуск теста");
-        }
-    }
-
-    interface IPrinter
-    {
-        void Print(string str);
-    }
-
-    class DebugPrinter : IPrinter
-    {
-        public DebugPrinter()
-        {
-            
-        }
-
-        public void Print(string str)
-        {
-            Debug.WriteLine(str);
         }
     }
 }

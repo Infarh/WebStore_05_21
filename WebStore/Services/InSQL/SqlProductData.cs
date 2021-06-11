91181857c3cbde12fc3cc5361d2dbaea8bd47ad5
Infarh <shmachilin@yandex.ru>
2021-06-11 21:33:41 +0300

    Извлечение товаров по заданному списку идентификаторов

--- WebStore/Services/InSQL/SqlProductData.cs
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -24,11 +24,16 @@ namespace WebStore.Services.InSQL
                .Include(p => p.Brand)
                .Include(p => p.Section);
 
-            if (Filter?.SectionId is { } section_id)
-                query = query.Where(product => product.SectionId == section_id);
+            if (Filter?.Ids?.Length > 0)
+                query = query.Where(product => Filter.Ids.Contains(product.Id));
+            else
+            {
+                if (Filter?.SectionId is { } section_id)
+                    query = query.Where(product => product.SectionId == section_id);
 
-            if (Filter?.BrandId is { } brand_id)
-                query = query.Where(product => product.BrandId == brand_id);
+                if (Filter?.BrandId is { } brand_id)
+                    query = query.Where(product => product.BrandId == brand_id);
+            }
 
             return query;
         }

9301e989cc4a068968b50e495e05160ff3d83f7b
Infarh <shmachilin@yandex.ru>
2021-06-11 21:11:22 +0300

    Заготовка сервиса корзины

---
+++ WebStore/Services/Interfaces/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using WebStore.Models;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int id);
+
+        void Decrement(int id);
+
+        void Remove(int id);
+
+        void Clear();
+
+        CartViewModel GetViewModel();
+    }
+}

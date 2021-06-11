7b1bae2e7cbc09172df1c1d83d6ffda21fee17d7
Infarh <shmachilin@yandex.ru>
2021-06-11 21:45:55 +0300

    Подготовка представления корзины

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService) => _CartService = CartService;
+
+        public IActionResult Index() => View(_CartService.GetViewModel());
+    }
+}

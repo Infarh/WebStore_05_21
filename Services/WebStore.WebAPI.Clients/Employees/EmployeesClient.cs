c2d9de8346f65c05e1678bb223dee13e80e94216
Infarh <shmachilin@yandex.ru>
2021-06-25 20:11:55 +0300

    Контроллер WebAPI и клиент к нему для управления сотрудниками

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(HttpClient Client) : base(Client, "api/employees") { }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return Get<IEnumerable<Employee>>(Address);
+        }
+
+        public Employee Get(int id)
+        {
+            return Get<Employee>($"{Address}/{id}");
+        }
+
+        public int Add(Employee employee)
+        {
+            var response = Post(Address, employee);
+            var id = response.Content.ReadFromJsonAsync<int>().Result;
+            return id;
+        }
+
+        public void Update(Employee employee)
+        {
+            Put(Address, employee);
+        }
+
+        public bool Delete(int id)
+        {
+            var result = Delete($"{Address}/{id}").IsSuccessStatusCode;
+            return result;
+        }
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_EmployeesData.GetAll());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_EmployeesData.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            var id = _EmployeesData.Add(employee);
+            return Ok(id);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Employee employee)
+        {
+            _EmployeesData.Update(employee);
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _EmployeesData.Delete(id);
+            return result ? Ok(true) : NotFound(false);
+        }
+    }
+}

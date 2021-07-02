b5aa15685bb557eecb9513242e922fc536b7a5c5
Infarh <shmachilin@yandex.ru>
2021-07-02 20:50:42 +0300

    Самописные логеры

---
+++ Common/WebStore.Logger/Log4NetLoggerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetLoggerFactory
+    {
+        private static string CheckFilePath(string FilePath)
+        {
+            if (FilePath is not { Length: > 0 })
+                throw new ArgumentException("Не указан путь к конфигурационному файлу");
+            if (Path.IsPathRooted(FilePath))
+                return FilePath;
+
+            var assembly = Assembly.GetEntryAssembly();
+            var dir = Path.GetDirectoryName(assembly!.Location);
+            return Path.Combine(dir!, FilePath);
+        }
+
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4net.config")
+        {
+            Factory.AddProvider(new Log4NetLoggerProvider(CheckFilePath(ConfigurationFile)));
+            return Factory;
+        }
+    }
+}
---
+++ Common/WebStore.Logger/Log4NetLoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.Xml;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLoggerProvider : ILoggerProvider
+    {
+        private readonly string _ConfigurationFile;
+        private readonly ConcurrentDictionary<string, Log4NetLogger> __Loggers = new ();
+
+        public Log4NetLoggerProvider(string ConfigurationFile) => _ConfigurationFile = ConfigurationFile;
+
+        public ILogger CreateLogger(string Category) =>
+            __Loggers.GetOrAdd(Category, category =>
+            {
+                var xml = new XmlDocument();
+                xml.Load(_ConfigurationFile);
+                return new Log4NetLogger(category, xml["log4net"]);
+            });
+
+        public void Dispose() => __Loggers.Clear();
+    }
+}

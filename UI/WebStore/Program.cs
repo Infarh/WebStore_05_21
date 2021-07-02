9a2a8e78873652342bef0e0321bc84db614a86cf
Infarh <shmachilin@yandex.ru>
2021-07-02 21:07:15 +0300

    Настройка Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,7 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore
 {
@@ -23,6 +27,15 @@ namespace WebStore
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
                 )
+              .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                  .MinimumLevel.Debug()
+                  .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                  .Enrich.FromLogContext()
+                  .WriteTo.Console(
+                       outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                  .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                  .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+               )
             ;
     }
 }

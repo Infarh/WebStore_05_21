af6d9d51514656fc38a81beafae7c9e568a5041b
Infarh <shmachilin@yandex.ru>
2021-07-09 20:29:55 +0300

    Подготовка к визуализаци хлебных крошек

---
+++ UI/WebStore/ViewModels/BreadCrumbsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.ViewModels
+{
+    public class BreadCrumbsViewModel
+    {
+        public Section Section { get; init; }
+
+        public Brand Brand { get; init; }
+
+        public string Product { get; init; }
+    }
+}

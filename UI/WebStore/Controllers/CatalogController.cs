4639f38e94f1b17d6ebf37104536c5e124fd6ac1
Infarh <shmachilin@yandex.ru>
2021-07-13 21:04:20 +0300

    Заготовка таг-хелпера переключалки страниц каталога товаров

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -32,13 +32,19 @@ namespace WebStore.Controllers
                 PageSize = page_size,
             };
 
-            var products = _ProductData.GetProducts(filter);
+            var (products, total_count) = _ProductData.GetProducts(filter);
 
             return View(new CatalogViewModel
             {
                 BrandId = BrandId,
                 SectionId = SectionId,
-                Products = products.Products.OrderBy(p => p.Order).ToView()
+                Products = products.OrderBy(p => p.Order).ToView(),
+                PageViewModel = new PageViewModel
+                {
+                    Page = Page,
+                    PageSize = page_size ?? 0,
+                    TotalItems = total_count,
+                },
             });
         }
 
---
+++ UI/WebStore/TagHelpers/Pageing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.TagHelpers
+{
+    public class Pageing : TagHelper
+    {
+        private readonly IUrlHelperFactory _UrlHelperFactory;
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PageViewModel PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        public Pageing(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+        }
+    }
+}

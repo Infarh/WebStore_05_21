ee0d9fcf8ab332b1386950ec95b3cc303c26c436
Infarh <shmachilin@yandex.ru>
2021-07-16 20:53:28 +0300

    Управление api-контроллерами с помощью ненавязчивого AJAX

---
+++ UI/WebStore/Controllers/API/ConsoleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsoleController : ControllerBase
+    {
+        [HttpGet("clear")]
+        public void Clear() => Console.Clear();
+
+        [HttpGet("WriteLine")]
+        public void WriteLine(string Message) => Console.WriteLine(Message);
+    }
+}

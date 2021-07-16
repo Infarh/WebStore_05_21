cf7eec288d9c1c2779a5cedd8d398f6718505742
Infarh <shmachilin@yandex.ru>
2021-07-16 19:42:49 +0300

    Логика скрипта переключения страниц каталога товаров

--- UI/WebStore/wwwroot/js/Catalog/CatalogLogik.js
+++ UI/WebStore/wwwroot/js/Catalog/CatalogLogik.js
@@ -14,6 +14,37 @@
 
         const button = $(this);
 
+        if (button.prop("href").length > 0) {
+            const page = button.data("page");
 
+            const container = $("#catalog-items-container");
+
+            container.LoadingOverlay("show");
+
+            let query = "";
+
+            const data = button.data();
+
+            for (let key in data)
+                if (data.hasOwnProperty(key))
+                    query += `${key}=${data[key]}&`;
+
+            $.get(Catalog._properties.getViewLink + "?" + query)
+                .done(catalogHtml => {
+                    container.html(catalogHtml);
+                    container.LoadingOverlay("hide");
+
+                    $(".pagination li").removeClass("active");
+                    $(".pagination li a").prop("href", "#");
+
+                    $(`.pagination li a[data-page=${page}]`)
+                        .removeAttr("href")
+                        .parent().addClass("active");
+                })
+                .fail(() => {
+                    container.LoadingOverlay("hide");
+                    console.log("clickOnPage fail");
+                });
+        }
     }
 }

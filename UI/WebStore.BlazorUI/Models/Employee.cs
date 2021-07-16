24933c7528482d6500f79be980f032659f8c5b6a
Infarh <shmachilin@yandex.ru>
2021-07-16 22:09:09 +0300

    Эксперимент

---
+++ UI/WebStore.BlazorUI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace WebStore.BlazorUI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString() => $"(id:{Id}) {LastName} {FirstName} {Patronymic} age:{Age}";
+    }
+}
